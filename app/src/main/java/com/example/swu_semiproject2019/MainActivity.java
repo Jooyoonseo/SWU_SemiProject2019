a44fbbdedbc6eea78b86e9d0e9152da404e91b6f
JooYoonSeo <ded05097@gmail.com>
2019-07-03 14:55:12 +0900

    첫번째 커밋이다.

--- app/src/main/java/com/example/swu_semiproject2019/MainActivity.java
+++ app/src/main/java/com/example/swu_semiproject2019/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //테스트 코드
+
+        //오늘 컨디션 중간
+
+        //커밋 및 푸시 테스트
+        
     }
 }
